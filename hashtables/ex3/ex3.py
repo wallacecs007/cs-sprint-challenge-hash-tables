fdc513451c2b69ca2fbe578ee8333e2aaabfba5f
Chase Wallace <wallacecs007@gmail.com>
2020-08-15 11:17:06 -0600

    ex3 completed, tests passing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,25 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Creating empty dict and list
+    dict = {}
+    result = []
+
+    # Looping through arrays provided
+    for array in arrays:
+        # Looping through numbers in each array
+        # While this is a nested loop, usually you would loop through another
+        # List while looping through a list. By using a dict, we can check for
+        # numbers that have been found already.
+        for number in array:
+            if number in dict.keys():
+                # If it exists, increment it
+                dict[number] += 1
+            else:
+                # Else add it
+                dict[number] = 1
+    length = len(arrays)
+    for key in dict.keys():
+        if dict[key] == length:
+            result.append(key)
 
     return result
 
