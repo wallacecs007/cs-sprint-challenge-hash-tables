8bd253b8afbcc20fe7f5761eb4565a7170280692
Chase Wallace <wallacecs007@gmail.com>
2020-08-15 11:08:33 -0600

    ex5 is complete, tests are passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,28 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Creating an empty list and dict
+    dict = {}
+    result = []
+
+    for file in files:
+        # Breaking the file out by the / divider for paths
+        split = file.split("/")
+        # Getting the last part of the file path which is the file name
+        # we are searching for
+        name = split[-1]
+
+        # Creating an array to store multiple file paths
+        if name not in dict.keys():
+            dict[name] = []
+
+        # Adding the filepath
+        dict[name].append(file)
+
+    # Going through every query, and seeing if it has files associated with it.
+    for query in queries:
+        if query in dict.keys():
+            result.extend(dict[query])
+
+    print(result)
 
     return result
 
