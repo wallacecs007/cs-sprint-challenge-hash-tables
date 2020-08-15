1177142d8ab85507d27a8a0aa3ca20cbbe86ec07
Chase Wallace <wallacecs007@gmail.com>
2020-08-15 10:18:22 -0600

    Finished Indices fix. Testing passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,21 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
     # Setting up an empty Dict
     dict = {}
-
+    # Defining an indice counter
+    indice = 0
+    # Looping over each weight
     for item in weights:
-        if item in dict.keys():
-            print(dict[item])
-            print(item)
-            return [dict[item], item]
+        # Check if the weight is in the hashtable that matches the current check
+        # If so return the indices, largest (newest item) first.
+        if limit - item in dict.keys():
+            return [indice, dict[limit - item]]
+        # Else add item to hashtable
         else:
-            dict[limit - item] = item
+            dict[item] = indice
+        indice += 1
 
+    # Nothing found, return none.
     return None
-
-
-weights_2 = [4, 4]
-answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
-print(answer_2)
-weights_3 = [4, 6, 10, 15, 16]
-answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
-print(answer_3)
