26718bf2069fbacc9d0944ab7d3307ad1e01cfd5
Chase Wallace <wallacecs007@gmail.com>
2020-08-15 10:13:42 -0600

    Finished ex1 logic, need to return indices instead of item tuple

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Setting up an empty Dict
+    dict = {}
+
+    for item in weights:
+        if item in dict.keys():
+            print(dict[item])
+            print(item)
+            return [dict[item], item]
+        else:
+            dict[limit - item] = item
 
     return None
+
+
+weights_2 = [4, 4]
+answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+print(answer_2)
+weights_3 = [4, 6, 10, 15, 16]
+answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
+print(answer_3)
